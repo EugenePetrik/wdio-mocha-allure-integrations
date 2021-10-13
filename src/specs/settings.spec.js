83c025b89bc4d8f60179fd10cc81fef497eefcd5
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-10-13 12:19:50 +0300

    Add test for Settings page

---
+++ src/specs/settings.spec.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import { SettingsPage } from '../page_objects/settings.page';
+import { createUserAndSignIn } from '../utils/api';
+import { user } from '../models';
+
+describe('Settings', () => {
+  let settingsPage;
+
+  before(async () => {
+    await createUserAndSignIn(user);
+  });
+
+  beforeEach(async () => {
+    settingsPage = new SettingsPage();
+
+    await settingsPage.open();
+    await settingsPage.waitSettingsPageLoaded();
+  });
+
+  it('should open the page', async () => {
+    const pageTitle = await settingsPage.getPageTitle();
+    expect(pageTitle).to.eq('Conduit');
+
+    const pageHeader = await settingsPage.getPageHeader();
+    expect(pageHeader).to.eq('Your Settings');
+
+    const getUsername = await settingsPage.getUsernameText();
+    expect(getUsername).to.eq(user.username);
+
+    const getEmail = await settingsPage.getEmailText();
+    expect(getEmail).to.eq(user.email);
+
+    const isUpdateSettingButtonDisplayed = await settingsPage.isUpdateSettingButtonDisplayed();
+    expect(isUpdateSettingButtonDisplayed).to.be.true;
+
+    const isLogOutButtonDisplayed = await settingsPage.isLogOutButtonDisplayed();
+    expect(isLogOutButtonDisplayed).to.be.true;
+  });
+});
